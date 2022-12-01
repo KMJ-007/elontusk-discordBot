eb6a6cefeb4f26662ff95cd07c9525fad726c6c8
knowOne08 <yashdarji23082004@gmail.com>
2022-12-01 10:55:39 +0530

    updates

--- src/index.js
+++ src/index.js
@@ -1,10 +1,27 @@
 const Discord = require('discord.js')
 const dotenv = require("dotenv")
-dotenv.config()
+const path = require('path');
+// require('dotenv').config({ path: path.resolve(__dirname, '../.env') })
+require('dotenv').config({path:'F:\\DiscordBot\\.env'})
 const client = new Discord.Client();
 
 client.once('ready', () => {
     console.log("ElonTusk is live !");
 });
 
+client.on('ready',() => {
+    console.log(`logged in as ${client.user.tag}`)
+});
+client.on('message', msg => {
+    if(msg.autor.bot) { return}
+
+
+    else if(msg.content.startsWith("hello")) {
+        msg.channel.send(`hello ${msg.author}`);
+    } 
+});
+// const DISCORD_TOKEN = process.env.TOKEN;
+// console.log(DISCORD_TOKEN);
+// console.log(DISCORD_TOKEN);
+//client.login(`'${DISCORD_TOKEN}'`);
 client.login(process.env.TOKEN);
