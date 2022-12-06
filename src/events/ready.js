a2d56fd0e727645c1c3a1d05483d6a3580bed226
knowOne08 <yashdarji23082004@gmail.com>
2022-12-06 13:39:53 +0530

    Updates

--- src/events/ready.js
+++
@@ -1,9 +0,0 @@
-const { Events } = require('discord.js');
-
-module.exports = {
-	name: 'ready',
-	once: true,
-	async execute(client) {
-		console.log(`Ready! Logged in as ${client.user.tag}`);
-	},
-};
