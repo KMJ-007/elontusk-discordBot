44f0e1290a0b56a416ff2ffbe64f7cb3eed0799f
knowOne08 <yashdarji23082004@gmail.com>
2022-12-09 11:42:35 +0530

    Feature creating thread beta

--- src/index.js
+++ src/index.js
@@ -3,10 +3,16 @@ const path = require("node:path");
 const { Client, Collection, Events, GatewayIntentBits } = require("discord.js");
 const { token } = require("./config.json");
 
-const client = new Client({ 
-    intents: [
-    GatewayIntentBits.Guilds
-] });
+const keepAlive = require('./serverWake.js');
+
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent,
+    GatewayIntentBits.GuildMembers,
+  ]
+});
 
 client.commands = new Collection();
 const commandsPath = path.join(__dirname, "commands");
@@ -16,10 +22,10 @@ for (const file of commandFiles) {
   const filePath = path.join(commandsPath, file);
   const command = require(filePath);
   // Set a new item in the Collection with the key as the command name and the value as the exported module
-  
+
   if ("data" in command && "execute" in command) {
     client.commands.set(command.data.name, command);
-} else {
+  } else {
     console.log(
       `[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`
     );
@@ -47,5 +53,43 @@ client.on(Events.InteractionCreate, async (interaction) => {
     });
   }
 });
+
+//hello message
+client.on("messageCreate", (message) => {
+  if (message.author.bot) return false;
+
+  if (message.content === "hello Freud") {
+    message.channel.send(`hi ${message.author.username}`);
+  }
+
+});
+
+
+//Apreciation Thread on commit 
+client.on('messageCreate', async (msg) => {
+  let cmtLnk = /https:\/\/github\.com\/.*\/.*\/commit\/[0-9a-f]{40}/;
+
+  if (msg.content.match(cmtLnk) !== null) {
+
+    msg.react('🔥')
+    const thread =  await msg.channel.threads.create({
+      name: "AppreciationThread",  
+    }); 
+    const threadId = thread.id;
+  
+
+    const webhooks = await msg.channel.fetchWebhooks();
+    const webhook = webhooks.first();
+
+    await webhook.send({
+      content: 'Damnn Bro, You Work too hard !!',
+      threadId: threadId,
+    });
+  }
+});
+
+
+
+keepAlive()
 
 client.login(token);
